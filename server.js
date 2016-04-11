d6397caeafb0fcb6b1267c64595a9b8a57c61f27
Nathan LaPré <nmlapre@users.noreply.github.com>
2016-04-10 21:04:40 -0700

    trying the express way of specifying access control

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ var Firebase = require("firebase");
 var qs = require('querystring');
 var dbref = new Firebase('https://walkwithwoof.firebaseio.com/');
 var fs = require('fs');
-// var cors = require('cors');
-// var app = express();
-// app.use(cors());
+var express = require('express');
+var app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://walkwithwoof.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 console.log("server started");
 
