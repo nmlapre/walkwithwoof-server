850a41d48a91570295f8b9738e13b3edcaa52598
Nathan LaPré <nmlapre@users.noreply.github.com>
2016-04-10 21:16:02 -0700

    moved  setHeaders around

--- server.js
+++ server.js
@@ -4,29 +4,6 @@ var Firebase = require("firebase");
 var qs = require('querystring');
 var dbref = new Firebase('https://walkwithwoof.firebaseio.com/');
 var fs = require('fs');
-var express = require('express');
-var app = express();
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://walkwithwoof.com/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 
 console.log("server started");
 
@@ -45,8 +22,16 @@ http.createServer(function (request, response) {
             //parse the received body data
             var post = qs.parse(body);
 
+            // let walkwithwoof.com POST
+            response.setHeader('Access-Control-Allow-Origin', 'http://walkwithwoof.com/');
+            response.setHeader('Access-Control-Allow-Methods', 'POST');
+            response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            
             //request ended -> do something with the data
-            response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+            response.writeHead(200, "OK", 
+                {'Content-Type': 'text/html',
+                'Access-Control-Allow-Origin': 'http://walkwithwoof.com'
+                );
             
             //output the decoded data to the HTTP response
             console.log(post.email);
